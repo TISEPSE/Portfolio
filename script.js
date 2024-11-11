e0246d158f6e508b5ae187751abef9d632cd25d6
TISEPSE <titi21player@gmail.com>
2024-11-11 22:45:33 +0100

    push modal css

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-// Sélectionnez votre première carte et la modale
-const firstCard = document.querySelector(".card")
-const modal = document.getElementById("myModal")
-const closeBtn = document.querySelector(".close")
+// Sélectionner tous les éléments nécessaires
+const cards = document.querySelectorAll(".card")
+const modal = document.getElementById("projectModal")
+const modalImage = modal.querySelector(".modal-image")
+const modalTitle = modal.querySelector(".modal-title")
+const modalDescription = modal.querySelector(".modal-description")
+const closeButton = modal.querySelector(".close")
 
-// Ouvrir la modale quand on clique sur la première carte
-firstCard.addEventListener("click", () => {
-  modal.classList.add("show")
+// Ajouter un écouteur d'événements à chaque carte
+cards.forEach(card => {
+  card.addEventListener("click", () => {
+    modalImage.src = card.querySelector("img").src
+    modalTitle.textContent = card.querySelector("h2").textContent
+    modalDescription.textContent = card.querySelector("p").textContent
+    modal.classList.add("show")
+  })
 })
 
-// Fermer la modale quand on clique sur le bouton de fermeture
-closeBtn.addEventListener("click", () => {
+// Fermer la modale lorsqu'on clique sur le bouton de fermeture
+closeButton.addEventListener("click", () => {
   modal.classList.remove("show")
 })
 
-// Fermer la modale si on clique en dehors du contenu
+// Fermer la modale lorsqu'on clique en dehors de son contenu
 window.addEventListener("click", event => {
   if (event.target === modal) {
     modal.classList.remove("show")
